c7a2ce8dc91cfa1e0fcaf00e5567bf78428fd66f
LindaLawton <linda_jess@hotmail.com>
2014-09-26 14:59:49 +0200

    Comments / examples
    Adding some comments and examples in how to use the helper classes

--- Google-Analytics/Daimto-Google-Analytics-Sample/Daimto-Google-Analytics-Sample/Program.cs
+++ Google-Analytics/Daimto-Google-Analytics-Sample/Daimto-Google-Analytics-Sample/Program.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using Google.Apis.Util.Store;
 using Google.Apis.Analytics.v3;
 using Google.Apis.Services;
+using Google.Apis.Analytics.v3.Data;
 
 //Target framework 4.0
 //Nuget package http://www.nuget.org/packages/Google.Apis.Analytics.v3
@@ -32,15 +33,82 @@ namespace Daimto_Google_Analytics_Sample
             //Service = DaimtoAnalyticsAuthenticationHelper.AuthenticateServiceAccount(SERVICE_ACCOUNT_EMAIL, SERVICE_ACCOUNT_KEYFILE);
 
 
-            // DaimtoAnaltyicsManagmentHelper.WebpropertyGet(Service, "1", "UA-45053573-1");
 
-            //DaimtoAnaltyicsManagmentHelper.WebpropertyInsert(Service, "45053573", "Test", "http:\\www.daimto.com");
-            //DaimtoAnaltyicsMetaDataHelper.GetDimensions(Service);
 
+            //Get account summary and display them.
+            foreach (AccountSummary account in DaimtoAnaltyicsManagmentHelper.AccountSummaryList(Service))
+            {
+                // Account
+                Console.WriteLine("Account: " + account.Name + "(" + account.Id + ")");
+
+                foreach (WebPropertySummary wp in account.WebProperties)
+                {
+
+                    // Web Properties within that account
+                    Console.WriteLine("\tWeb Property: " + wp.Name + "(" + wp.Id + ")");
+
+                    //Don't forget to check its not null. Believe it or not it could be.  
+                    if (wp.Profiles != null)
+                    {
+
+                        foreach (ProfileSummary profile in wp.Profiles)
+                        {
+                            // Profiles with in that web property.
+                            Console.WriteLine("\t\tProfile: " + profile.Name + "(" + profile.Id + ")");
+                        }
+                    }
+                }
+            }
+         
+
+            //This will also display a list to the user, you will get more information in an Account,
+            // Web property, and profile objects then you do from the same summary objects
+            //This will require a large number of requests to be sent against the API, be mindful of 
+            //  your quota (10000 requests per profile(view) per day) if you use this method.   
+            foreach (Account account in DaimtoAnaltyicsManagmentHelper.AccountList(Service))
+            {
+
+                // Account
+                Console.WriteLine("Account: " + account.Name + "(" + account.Id + ")");
+                foreach (Webproperty wp in DaimtoAnaltyicsManagmentHelper.WebpropertyList(Service, account.Id))
+                {
+                    // Web Properties within that account
+                    Console.WriteLine("\tWeb Property: " + wp.Name + "(" + wp.Id + ")");
+
+                    foreach (Profile profile in DaimtoAnaltyicsManagmentHelper.ProfileList(Service, account.Id, wp.Id))
+                    {
+
+                        Console.WriteLine("\t\tProfile: " + profile.Name + "(" + profile.Id + ")");
+                    }
+                }
+            }
+
+           
+            //Getting a list of the Dimensions and metrics.
+
+            string lastGroup = string.Empty;
+            string lastType = string.Empty;
+            Columns metadata = DaimtoAnaltyicsMetaDataHelper.MetaDataList(Service);
+            foreach (Column column in metadata.Items.OrderBy(a => a.Attributes["group"]).OrderBy(a => a.Attributes["type"]).ToList())
+            {
+                if (column.Attributes["group"] != lastGroup)
+                {
+                    Console.WriteLine("Group: " + column.Attributes["group"]);
+                    lastGroup = column.Attributes["group"];
+                    lastType = string.Empty;
+                }
+                if (column.Attributes["type"] != lastType)
+                {
+                    Console.WriteLine("\t" + column.Attributes["type"]);
+                    lastType = column.Attributes["type"];
+                }
+
+                Console.WriteLine("\t\t" + column.Attributes["uiName"] + " : " + column.Id);
+            }
+            
             DaimtoAnaltyicsReportingHelper.optionalValues options = new DaimtoAnaltyicsReportingHelper.optionalValues();
-
             options.Dimensions = "ga:date";
-
+            //Make sure the profile id you send is valid.  
             var x = DaimtoAnaltyicsReportingHelper.get(Service, "78110423", "10daysAgo", "today", "ga:sessions", options);
 
 
