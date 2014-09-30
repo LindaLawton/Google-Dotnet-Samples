9d444a506c91834ddc1bc98272e291d99f407dfb
LindaLawton <linda_jess@hotmail.com>
2014-09-30 10:20:39 +0200

    Fixed bug
    Bug in the Get method profileid must include ga:

--- Google-Analytics/Daimto-Google-Analytics-Sample/Daimto-Google-Analytics-Sample/DaimtoAnaltyicsRealTimeHelper.cs
+++ Google-Analytics/Daimto-Google-Analytics-Sample/Daimto-Google-Analytics-Sample/DaimtoAnaltyicsRealTimeHelper.cs
@@ -1,15 +1,70 @@
-﻿using System;
+﻿/*
+ * Copyright 2014 Daimto.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Google.Apis.Analytics.v3;
 using Google.Apis.Analytics.v3.Data;
-
 namespace Daimto_Google_Analytics_Sample
 {
     public class DaimtoAnaltyicsRealTimeHelper
     {
+        /// <summary>
+        /// There are several query Parameters that are optional this will allow you to send the ones you want.
+        /// </summary>
+        public class OptionalValues
+        {
+            private string dimensions { get; set; }
+            private string filter { get; set; }
+            private string sort { get; set; }
+            private string segment { get; set; }
+            private int maxResults { get; set; }
+            private DataResource.GaResource.GetRequest.SamplingLevelEnum sampleingLevel = DataResource.GaResource.GetRequest.SamplingLevelEnum.DEFAULT;
 
+
+            /// <summary>
+            /// A list of comma-separated dimensions for your Analytics data, such as ga:browser,ga:city. 
+            /// Documentation: https://developers.google.com/analytics/devguides/reporting/core/v3/reference#dimensions
+            /// </summary>            
+            public string Dimensions { get { return dimensions; } set { dimensions = value; } }
+
+            /// <summary>
+            /// Dimension or metric filters that restrict the data returned for your request. 
+            /// Documentation: https://developers.google.com/analytics/devguides/reporting/core/v3/reference#filters
+            /// </summary>
+            public string Filter { get { return filter; } set { filter = value; } }
+
+            /// <summary>
+            /// A list of comma-separated dimensions and metrics indicating the sorting order and sorting direction for the returned data. 
+            /// Documentation:  https://developers.google.com/analytics/devguides/reporting/core/v3/reference#sort
+            /// </summary>
+            public string Sort { get { return sort; } set { sort = value; } }
+
+            /// <summary>
+            /// Constructor sets up the default values, for things that can't be null.
+            /// </summary>
+            public OptionalValues()
+            {
+                this.dimensions = null;
+                this.filter = null;
+                this.sort = null;
+                this.segment = null;
+            }
+        }
         /// <summary>
         /// Returns real-time Google Analytics data for a view (profile).
         /// https://developers.google.com/analytics/devguides/reporting/realtime/v3/reference/data/realtime/get
@@ -23,39 +78,38 @@ namespace Daimto_Google_Analytics_Sample
         /// Documentation: Dimension and metric reference https://developers.google.com/analytics/devguides/reporting/realtime/dimsmets/
         /// </summary>
         /// <param name="service">Valid authenticated Google analytics service</param>
-        /// <param name="ProfileId">Profile id to request data from </param>
+        /// <param name="profileId">Profile id to request data from </param>
         /// <param name="metrics">Valid Real time Metrics (Required)</param>
-        /// <param name="dimensions">Valid real time dimensions</param>
-        /// <param name="filters">filters to be applied to real-time data. https://developers.google.com/analytics/devguides/reporting/core/v3/reference#filters</param>
-        /// <param name="sort">sort order for real-time data https://developers.google.com/analytics/devguides/reporting/core/v3/reference#sort</param>
+        /// <param name="optionalValues">Optional values can be null</param>
         /// <returns>https://developers.google.com/analytics/devguides/reporting/realtime/v3/reference/data/realtime#resource</returns>
-        public static RealtimeData RealTimeGet(AnalyticsService service, string ProfileId, string metrics, String dimensions, string filters, string sort)
+        public static RealtimeData Get(AnalyticsService service, string profileId, string metrics, OptionalValues optionalValues)
         {
-
-
-            DataResource.RealtimeResource.GetRequest list = service.Data.Realtime.Get(ProfileId, metrics);
-            list.MaxResults = 10000;
-
-
-            if (!string.IsNullOrEmpty(dimensions))
+            try
             {
-                list.Dimensions = dimensions;
-            }
 
-            if (!string.IsNullOrEmpty(filters))
+                DataResource.RealtimeResource.GetRequest request = service.Data.Realtime.Get(String.Format("ga:{0}", profileId), metrics);
+                request.MaxResults = 10000;
+
+
+                if (optionalValues != null)
+                {
+                    request.Dimensions = optionalValues.Dimensions;
+                    request.Sort = optionalValues.Sort;
+                    request.Filters = optionalValues.Filter;
+                }
+
+
+                RealtimeData feed = request.Execute();
+
+                return feed;
+            }
+            catch (Exception ex)
             {
-                list.Dimensions = filters;
+
+                Console.WriteLine(ex.Message);
+                return null;
+
             }
-            if (!string.IsNullOrEmpty(sort))
-            {
-                list.Dimensions = sort;
-            }
-
-       
-            RealtimeData Feed = list.Execute();
-
-
-            return Feed;
 
 
         }
