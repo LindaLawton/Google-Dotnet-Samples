d53d45298c65b43867df15865626543712664c19
LindaLawton <linda_jess@hotmail.com>
2014-09-30 10:20:07 +0200

    licence
    Added licence at the top

--- Google-Analytics/Daimto-Google-Analytics-Sample/Daimto-Google-Analytics-Sample/DaimtoAnaltyicsMetaDataHelper.cs
+++ Google-Analytics/Daimto-Google-Analytics-Sample/Daimto-Google-Analytics-Sample/DaimtoAnaltyicsMetaDataHelper.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+ * Copyright 2014 Daimto.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -59,7 +74,7 @@ namespace Daimto_Google_Analytics_Sample
         /// </summary>
         /// <param name="service">Valid Analytics Service</param>
         /// <returns></returns>
-        public static Columns GetSearchattributes(AnalyticsService service, String attributeName, string attributeValue)
+        public static Columns GetSearchattributes(AnalyticsService service, string attributeName, string attributeValue)
         {
             Columns result = DaimtoAnaltyicsMetaDataHelper.MetaDataList(service);
             result.Items = result.Items.Where(a => a.Attributes[attributeName] == attributeValue).ToList();
