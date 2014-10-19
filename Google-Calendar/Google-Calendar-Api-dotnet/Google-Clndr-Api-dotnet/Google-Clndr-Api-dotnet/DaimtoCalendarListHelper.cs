96bed355d8310dfd24fc40df2a71460bb9b17c89
Linda Lawton <support@daimto.com>
2014-10-19 19:57:36 +0200

    init

---
+++ Google-Calendar/Google-Calendar-Api-dotnet/Google-Clndr-Api-dotnet/Google-Clndr-Api-dotnet/DaimtoCalendarListHelper.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+
+namespace Google_Clndr_Api_dotnet
+{
+    class DaimtoCalendarListHelper
+    {
+
+         /// <summary>
+        /// There are several query Parameters that are optional this will allow you to send the ones you want.
+        /// </summary>
+        public class OptionalValues
+        {
+            private Boolean showDeleted { get; set; }
+            private Boolean showHidden { get; set; }
+            private int maxResults { get; set; }
+            private CalendarListResource.ListRequest.MinAccessRoleEnum? minAccessRole = null;
+
+
+            /// <summary>
+            /// Whether to include deleted calendar list entries in the result. Optional. The default is False. 
+            /// Documentation: https://developers.google.com/google-apps/calendar/v3/reference/calendarList/list
+            /// </summary>            
+            public Boolean ShowDeleted { get { return showDeleted; } set { showDeleted = value; } }
+
+            /// <summary>
+            /// Dimension or metric filters that restrict the data returned for your request. 
+            /// Documentation: https://developers.google.com/google-apps/calendar/v3/reference/calendarList/list
+            /// </summary>
+            public Boolean ShowHidden { get { return showHidden; } set { showHidden = value; } }
+
+            /// <summary>
+            /// Maximum number of entries returned on one result page. By default the value is 100 entries. The page size can never be larger than 250 entries. Optional. 
+            /// Documentation: https://developers.google.com/google-apps/calendar/v3/reference/calendarList/list
+            /// </summary>
+            public int MaxResults { get { return maxResults; } set { maxResults = value; } }
+
+
+            /// <summary>
+            /// The minimum access role for the user in the returned entires. Optional. The default is no restriction. 
+            ///Acceptable values are: •"freeBusyReader": The user can read free/busy information. 
+            ///•"owner": The user can read and modify events and access control lists. 
+            ///•"reader": The user can read events that are not private. 
+            ///•"writer": The user can read and modify events. 
+            /// Documentation: https://developers.google.com/google-apps/calendar/v3/reference/calendarList/list
+            /// </summary>
+            public CalendarListResource.ListRequest.MinAccessRoleEnum? MinAccessRole { get { return minAccessRole; } set { minAccessRole = value; } }
+
+            /// <summary>
+            /// Constructor sets up the default values, for things that can't be null.
+            /// </summary>
+            public OptionalValues()
+            {
+                this.maxResults = 100;
+                this.showDeleted = false;
+                this.showHidden = false;
+                this.minAccessRole = null;
+            }
+        }
+
+
+        public static void list(CalendarService service, OptionalValues optionalValues)
+        {
+
+            var request = service.CalendarList.List();
+
+
+            if (optionalValues == null)
+            {
+                request.MaxResults = 100;
+            }
+            else
+            {
+                request.MaxResults = optionalValues.MaxResults;
+                request.ShowDeleted = optionalValues.ShowDeleted;
+                request.ShowHidden = optionalValues.ShowHidden;
+                request.MinAccessRole = optionalValues.MinAccessRole;
+            }
+
+            CalendarList l =ProcessResults( request);
+       
+        
+        }
+
+
+        // Just loops though getting all the rows.  
+        private static CalendarList ProcessResults(CalendarListResource.ListRequest request)
+        {
+            try
+            {
+                CalendarList result = request.Execute();
+                List<CalendarListEntry> allRows = new List<CalendarListEntry>();
+
+                //// Loop through until we arrive at an empty page
+                while (result.Items != null)
+                {
+                    //Add the rows to the final list
+                    allRows.AddRange(result.Items);
+
+                    // We will know we are on the last page when the next page token is
+                    // null.
+                    // If this is the case, break.
+                    if (result.NextPageToken == null)
+                    {
+                        break;
+                    }
+                     // Prepare the next page of results
+                    request.PageToken = result.NextPageToken;
+
+                    // Execute and process the next page request
+                    result = request.Execute();
+
+                }
+                CalendarList allData = result;
+                allData.Items = (List<CalendarListEntry>)allRows;
+                return allData;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+
+
+
+
+        }
+
+
+    }
+
+   
+}
---
+++ Google-Calendar/Google-Calendar-Api-dotnet/Google-Clndr-Api-dotnet/Google-Clndr-Api-dotnet/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Google.Apis.Auth.OAuth2;
+using System.Threading;
+using Google.Apis.Util.Store;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+
+
+//Nuget package https://www.nuget.org/packages/Google.Apis.Calendar.v3/
+//PM> Install-Package Google.Apis.Calendar.v3
+
+
+namespace Google_Clndr_Api_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] scopes = new string[] {
+        CalendarService.Scope.Calendar  ,  // Manage your calendars
+        CalendarService.Scope.CalendarReadonly    // View your Calendars
+            };
+
+            // Authenticate Oauth2
+            String CLIENT_ID = "1046123799103-7mk8g2iok1dv9fphok8v2kv82hiqb0q6.apps.googleusercontent.com";
+            String CLIENT_SECRET = "GeE-cD7PtraV0LqyoxqPnOpv";
+            // here is where we Request the user to give us access, or use the Refresh Token that was previously stored in %AppData%
+            UserCredential credential = GoogleWebAuthorizationBroker.AuthorizeAsync(new ClientSecrets { ClientId = CLIENT_ID, ClientSecret = CLIENT_SECRET }
+                                                                , scopes
+                                                                , Environment.UserName
+                                                                , CancellationToken.None
+                                                                , new FileDataStore("Daimto.GoogleCalendar.Auth.Store")).Result;
+
+
+
+            CalendarService service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Calendar API Sample",
+            });
+
+            DaimtoCalendarListHelper.list(service, null);
+
+            int i = 1;
+
+
+        }
+    }
+}
