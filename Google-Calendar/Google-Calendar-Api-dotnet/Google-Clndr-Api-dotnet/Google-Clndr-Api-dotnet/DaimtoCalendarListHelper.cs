bfdd6ab777fe0320b88cc9e1e1728e163ee13c37
Linda Lawton <support@daimto.com>
2014-11-09 20:02:45 +0100

    get, udate, patch, watch, insert and delete added

--- Google-Calendar/Google-Calendar-Api-dotnet/Google-Clndr-Api-dotnet/Google-Clndr-Api-dotnet/DaimtoCalendarListHelper.cs
+++ Google-Calendar/Google-Calendar-Api-dotnet/Google-Clndr-Api-dotnet/Google-Clndr-Api-dotnet/DaimtoCalendarListHelper.cs
@@ -10,7 +10,9 @@ namespace Google_Clndr_Api_dotnet
     class DaimtoCalendarListHelper
     {
 
-         /// <summary>
+        #region List
+
+        /// <summary>
         /// There are several query Parameters that are optional this will allow you to send the ones you want.
         /// </summary>
         public class OptionalValues
@@ -81,9 +83,9 @@ namespace Google_Clndr_Api_dotnet
                 request.MinAccessRole = optionalValues.MinAccessRole;
             }
 
-            CalendarList l =ProcessResults( request);
-       
-        
+            CalendarList l = ProcessResults(request);
+
+
         }
 
 
@@ -108,7 +110,7 @@ namespace Google_Clndr_Api_dotnet
                     {
                         break;
                     }
-                     // Prepare the next page of results
+                    // Prepare the next page of results
                     request.PageToken = result.NextPageToken;
 
                     // Execute and process the next page request
@@ -124,14 +126,170 @@ namespace Google_Clndr_Api_dotnet
                 Console.WriteLine(ex.Message);
                 return null;
             }
+        }
+        #endregion
 
+        #region Get
 
-
-
+        /// <summary>
+        /// Returns an entry on the user's calendar list. 
+        /// Documentation:https://developers.google.com/google-apps/calendar/v3/reference/calendarList/get
+        /// </summary>
+        /// <param name="service">Valid Autenticated Calendar service</param>
+        /// <param name="id">Calendar identifier.</param>
+        /// <returns>CalendarList resorce: https://developers.google.com/google-apps/calendar/v3/reference/calendarList#resource </returns>
+        public static CalendarListEntry get(CalendarService service, string id)
+        {
+            try
+            {
+                return service.CalendarList.Get(id).Execute();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
         }
 
 
+        #endregion
+
+        #region patch
+
+
+
+        /// <summary>
+        /// Updates an entry on the user's calendar list.
+        /// Documentation:https://developers.google.com/google-apps/calendar/v3/reference/calendarList/patch
+        /// </summary>
+        /// <param name="service">Valid Autenticated Calendar service</param>
+        /// <param name="id">Calendar identifier.</param>
+        /// <param name="body">Changes you want to make:  Use var body = DaimtoCalendarListHelper.get(service,id);  
+        ///                    to get body then change that and pass it to the method.</param>
+        /// <returns>CalendarList resorce: https://developers.google.com/google-apps/calendar/v3/reference/calendarList#resource </returns>
+        public static CalendarListEntry patch(CalendarService service, string id, CalendarListEntry body)
+        {
+
+            try
+            {
+                return service.CalendarList.Patch(body, id).Execute();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+
+        #endregion
+
+        #region update
+
+        /// <summary>
+        /// Updates an entry on the user's calendar list.
+        /// Documentation:https://developers.google.com/google-apps/calendar/v3/reference/calendarList/update
+        /// </summary>
+        /// <param name="service">Valid Autenticated Calendar service</param>
+        /// <param name="id">Calendar identifier.</param>
+        /// <param name="body">Changes you want to make:  Use var body = DaimtoCalendarListHelper.get(service,id);  
+        ///                    to get body then change that and pass it to the method.</param>
+        /// <returns>CalendarList resorce: https://developers.google.com/google-apps/calendar/v3/reference/calendarList#resource </returns>
+        public static CalendarListEntry update(CalendarService service, string id, CalendarListEntry body)
+        {
+
+            try
+            {
+                return service.CalendarList.Update(body, id).Execute();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+
+        #endregion
+
+
+        #region watch
+
+        /// <summary>
+        /// Watch for changes to CalendarList resources.
+        /// Documentation:https://developers.google.com/google-apps/calendar/v3/reference/calendarList/watch
+        /// </summary>
+        /// <param name="service">Valid Autenticated Calendar service</param>
+        /// <param name="channel">Channel definaing what is to be watched.</param>
+        /// <returns>Channel being watched </returns>
+        public static Channel watch(CalendarService service, Channel channel)
+        {
+
+            try
+            {
+                return service.CalendarList.Watch(channel).Execute();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+
+        #endregion
+        #region Insert
+
+        /// <summary>
+        /// Adds an entry to the user's calendar list.  
+        /// Documentation:https://developers.google.com/google-apps/calendar/v3/reference/calendarList/insert
+        /// </summary>
+        /// <param name="service">Valid Autenticated Calendar service</param>
+        /// <param name="id">Calendar identifier.</param>
+        /// <returns>CalendarList resorce: https://developers.google.com/google-apps/calendar/v3/reference/calendarList#resource </returns>
+        public static CalendarListEntry insert(CalendarService service, CalendarListEntry body)
+        {
+            try
+            {
+                return service.CalendarList.Insert(body).Execute();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+
+        #endregion
+
+
+        #region Delete
+
+        /// <summary>
+        /// Deletes an entry on the user's calendar list.
+        /// Documentation:https://developers.google.com/google-apps/calendar/v3/reference/calendarList/delete
+        /// </summary>
+        /// <param name="service">Valid Autenticated Calendar service</param>
+        /// <param name="id">Calendar identifier.</param>
+        /// <returns>If successful, this method returns an empty response body. </returns>
+        public static string delete(CalendarService service, string id)
+        {
+            try
+            {
+                return service.CalendarList.Delete(id).Execute();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+
+        #endregion
+
     }
 
-   
+
 }
