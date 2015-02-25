a08b4be35f7dba3e631d283638344a335f7c6d06
Linda Lawton <support@daimto.com>
2015-02-25 20:26:04 +0100

    Adding bigQuery
    first upload

---
+++ Google-BigQuery/Google-BigQuery-Sample/Google-BigQuery-Sample/Authentication.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Google.Apis.Bigquery.v2;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Google.Apis.Auth.OAuth2;
+using System.Threading;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace Google_BigQuery_Sample
+{
+  public  class Authentication
+    {
+
+        /// <summary>
+        /// Authenticate to Google Using Oauth2
+        /// Documentation https://developers.google.com/accounts/docs/OAuth2
+        /// </summary>
+        /// <param name="clientId">From Google Developer console https://console.developers.google.com</param>
+        /// <param name="clientSecret">From Google Developer console https://console.developers.google.com</param>
+        /// <param name="userName">A string used to identify a user.</param>
+        /// <returns></returns>
+        public static BigqueryService  AuthenticateOauth(string clientId, string clientSecret, string userName)
+        {
+
+            string[] scopes = new string[] { BigqueryService.Scope.Bigquery,                // view and manage your BigQuery data
+                                             BigqueryService.Scope.BigqueryInsertdata ,     // Insert Data into Big query
+                                             BigqueryService.Scope.CloudPlatform,           // view and manage your data acroos cloud platform services
+                                             BigqueryService.Scope.DevstorageFullControl,   // manage your data on Cloud platform services
+                                             BigqueryService.Scope.DevstorageReadOnly ,     // view your data on cloud platform servies
+                                             BigqueryService.Scope.DevstorageReadWrite };   // manage your data on cloud platform servies
+
+            try
+            {
+                // here is where we Request the user to give us access, or use the Refresh Token that was previously stored in %AppData%
+                UserCredential credential = GoogleWebAuthorizationBroker.AuthorizeAsync(new ClientSecrets { ClientId = clientId, ClientSecret = clientSecret }
+                                                                                             , scopes
+                                                                                             , userName
+                                                                                             , CancellationToken.None
+                                                                                             , new FileDataStore("Daimto.BigQuery.Auth.Store")).Result;
+
+                BigqueryService service = new BigqueryService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "BigQuery API Sample",
+                });
+                return service;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.InnerException);
+                return null;
+
+            }
+
+        }
+
+        /// <summary>
+        /// Authenticating to Google using a Service account
+        /// Documentation: https://developers.google.com/accounts/docs/OAuth2#serviceaccount
+        /// </summary>
+        /// <param name="serviceAccountEmail">From Google Developer console https://console.developers.google.com</param>
+        /// <param name="keyFilePath">Location of the Service account key file downloaded from Google Developer console https://console.developers.google.com</param>
+        /// <returns></returns>
+        public static BigqueryService AuthenticateServiceAccount(string serviceAccountEmail, string keyFilePath)
+        {
+
+            // check the file exists
+            if (!File.Exists(keyFilePath))
+            {
+                Console.WriteLine("An Error occurred - Key file does not exist");
+                return null;
+            }
+
+            string[] scopes = new string[] { BigqueryService.Scope.Bigquery,                // view and manage your BigQuery data
+                                             BigqueryService.Scope.BigqueryInsertdata ,     // Insert Data into Big query
+                                             BigqueryService.Scope.CloudPlatform,           // view and manage your data acroos cloud platform services
+                                             BigqueryService.Scope.DevstorageFullControl,   // manage your data on Cloud platform services
+                                             BigqueryService.Scope.DevstorageReadOnly ,     // view your data on cloud platform servies
+                                             BigqueryService.Scope.DevstorageReadWrite };   // manage your data on cloud platform servies
+       
+
+            var certificate = new X509Certificate2(keyFilePath, "notasecret", X509KeyStorageFlags.Exportable);
+            try
+            {
+                ServiceAccountCredential credential = new ServiceAccountCredential(
+                    new ServiceAccountCredential.Initializer(serviceAccountEmail)
+                    {
+                        Scopes = scopes
+                    }.FromCertificate(certificate));
+
+                // Create the service.
+                BigqueryService service = new BigqueryService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "BigQuery API Sample",
+                });
+                return service;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.InnerException);
+                return null;
+
+            }
+        }
+    }
+}
---
+++ Google-BigQuery/Google-BigQuery-Sample/Google-BigQuery-Sample/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Google.Apis.Services;
+using Google.Apis.Bigquery.v2;
+using Google.Apis.Bigquery.v2.Data;
+namespace Google_BigQuery_Sample
+{
+    class Program
+    {
+        // https://www.nuget.org/packages/Google.Apis.Bigquery.v2/
+        // Install-Package Google.Apis.Bigquery.v2
+        static void Main(string[] args)
+        {
+            BigqueryService service;
+            // Authenticate Oauth2
+            String CLIENT_ID = "1046123799103-d0vpdthl4ms0soutcrpe036ckqn7rfpn.apps.googleusercontent.com";
+            String CLIENT_SECRET = "NDmluNfTgUk6wgmy7cFo64RV";
+            service = Authentication.AuthenticateOauth(CLIENT_ID, CLIENT_SECRET, "test");
+
+
+            var project = listProjects(service).Projects.Where(a=> a.Id=="1046123799103").FirstOrDefault();
+
+            var dataSets = Datasets.list(service, project.Id, null);
+
+
+
+
+            //foreach (var x in listProjects(service).Projects.Where(a=> a.Id=="").FirstOrDefault())
+            //{
+            //    // 1046123799103
+            //    int i = 1;
+
+            //    Console.WriteLine(x.FriendlyName );
+                
+            //    Console.WriteLine(l.Datasets.Count());
+            //}
+
+
+            // Run the request.
+            Console.WriteLine("Executing a list request...");
+            //var result = await service.Apis.List().ExecuteAsync();
+        }
+
+
+
+        /// <summary>
+        /// Documentation: https://cloud.google.com/bigquery/docs/reference/v2/projects/list
+        /// </summary>
+        /// <param name="service"></param>
+        /// <returns></returns>
+        public static ProjectList listProjects(BigqueryService service)
+        {
+
+            var request = service.Projects.List();
+
+
+
+            request.MaxResults = 100;
+
+
+
+            return ProcessResults(request);
+
+
+        }
+
+
+        // Just loops though getting all the rows.  
+        private static ProjectList ProcessResults(ProjectsResource.ListRequest request)
+        {
+            try
+            {
+                ProjectList result = request.Execute();
+
+
+                List<ProjectList.ProjectsData> allRows = new List<ProjectList.ProjectsData>();
+
+                //// Loop through until we arrive at an empty page
+                while (result.Projects != null)
+                {
+                    //Add the rows to the final list
+                    allRows.AddRange(result.Projects);
+
+                    // We will know we are on the last page when the next page token is
+                    // null.
+                    // If this is the case, break.
+                    if (result.NextPageToken == null)
+                    {
+                        break;
+                    }
+                    // Prepare the next page of results
+                    request.PageToken = result.NextPageToken;
+
+                    // Execute and process the next page request
+                    result = request.Execute();
+
+                }
+                ProjectList allData = result;
+                allData.Projects = (List<ProjectList.ProjectsData>)allRows;
+                return allData;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
