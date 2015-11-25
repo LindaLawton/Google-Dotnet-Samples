2215ac1b6401a945d20c904a9efb566293ce2124
Linda Lawton <linda_l@targit.com>
2015-11-25 20:13:23 +0100

    adding Analyticsv3Sample

---
+++ Analyticsv3Sample/AnalyticsSampleProject/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAIMTO.Analytics.v3.Sample
+{
+    /// <summary>
+    /// View and manage your Google Analytics data
+    /// https://developers.google.com/analytics/
+    /// In order to run this you must install the nuget package.  Please consult the readme.
+    /// 
+    /// Link to Nuget Package: https://www.nuget.org/packages/Google.Apis.analytics.v3/
+    /// PM> Install-Package Google.Apis.Analytics.v3 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+        }
+    }
+}
