15200259d19bfe744953551eb389e770b321739d
Linda Lawton <linda_l@targit.com>
2015-11-30 22:26:18 +0100

    fixes

--- Analyticsv3Sample/AnalyticsSampleProject/Program.cs
+++ Analyticsv3Sample/AnalyticsSampleProject/Program.cs
@@ -7,19 +7,17 @@ using System.Threading.Tasks;
 namespace DAIMTO.Analytics.v3.Sample
 {
     /// <summary>
-    /// View and manage your Google Analytics data
-    /// https://developers.google.com/analytics/
+	/// View and manage your Google Analytics data
+	/// https://developers.google.com/analytics/
     /// In order to run this you must install the nuget package.  Please consult the readme.
     /// 
-    /// Link to Nuget Package: https://www.nuget.org/packages/Google.Apis.analytics.v3/
+	/// Link to Nuget Package: https://www.nuget.org/packages/Google.Apis.analytics.v3/
     /// PM> Install-Package Google.Apis.Analytics.v3 
     /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-
-           
         }
     }
 }
