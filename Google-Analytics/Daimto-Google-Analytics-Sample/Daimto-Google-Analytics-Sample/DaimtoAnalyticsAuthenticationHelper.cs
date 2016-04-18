e3ca73c2c4213a6ef737d07c6063448ef745eaf0
Aaron Kim <ahkim@ahkim.com>
2016-04-18 17:15:31 +0800

    Update DaimtoAnalyticsAuthenticationHelper.cs
    Add AuthenticateReportingServiceAccount function. This is to create AnalyticsReportingService instance to use v4 of Reporting service.

--- Google-Analytics/Daimto-Google-Analytics-Sample/Daimto-Google-Analytics-Sample/DaimtoAnalyticsAuthenticationHelper.cs
+++ Google-Analytics/Daimto-Google-Analytics-Sample/Daimto-Google-Analytics-Sample/DaimtoAnalyticsAuthenticationHelper.cs
@@ -21,6 +21,7 @@ using Google.Apis.Analytics.v3;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
+using Google.Apis.AnalyticsReporting.v4;
 
 namespace Daimto.Google.Sample.Analytics
 {
@@ -117,7 +118,50 @@ namespace Daimto.Google.Sample.Analytics
             }
         }
 
+        /// <summary>
+        /// Authenticating to Google using a Service account
+        /// Documentation: https://developers.google.com/accounts/docs/OAuth2#serviceaccount
+        /// </summary>
+        /// <param name="serviceAccountEmail">From Google Developer console https://console.developers.google.com</param>
+        /// <param name="keyFilePath">Location of the Service account key file downloaded from Google Developer console https://console.developers.google.com</param>
+        /// <returns>instance of AnalyticsReportingService</returns>
+        public static AnalyticsReportingService AuthenticateReportingServiceAccount(string serviceAccountEmail, string keyFilePath)
+        {
+            // check the file exists
+            if (!File.Exists(keyFilePath))
+            {
+                Console.WriteLine("An Error occurred - Key file does not exist");
+                return null;
+            }
 
+            string[] scopes = new string[] { AnalyticsReportingService.Scope.Analytics,  // view and manage your analytics data
+                                             AnalyticsReportingService.Scope.AnalyticsReadonly};     // View analytics data            
+
+            var certificate = new X509Certificate2(keyFilePath, "notasecret", X509KeyStorageFlags.Exportable);
+            try
+            {
+                ServiceAccountCredential credential = new ServiceAccountCredential(
+                    new ServiceAccountCredential.Initializer(serviceAccountEmail)
+                    {
+                        Scopes = scopes
+                    }.FromCertificate(certificate));
+
+                // Create the service.
+                AnalyticsReportingService service = new AnalyticsReportingService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Reporting API Sample",
+                });
+                return service;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.InnerException);
+                return null;
+
+            }
+        }
 
     }
 }
